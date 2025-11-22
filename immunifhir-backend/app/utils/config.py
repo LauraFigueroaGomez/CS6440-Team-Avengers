109bc85a2982e560f0f4557c44e1a0d2717a3d2c
cvillarroel3 <crivilla@gatech.edu>
2025-11-21 22:38:16 -0500

    fix for config 3

--- immunifhir-backend/app/utils/config.py
+++ immunifhir-backend/app/utils/config.py
@@ -1,6 +1,5 @@
 import os
-import httpx
-from supabase import create_client, ClientOptions
+from supabase import create_client
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,13 +7,4 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-http_client = httpx.Client(http2=False, timeout=10.0)
-
-options = ClientOptions(
-    http_client=http_client,
-    postgrest_client_timeout=10,
-    storage_client_timeout=10,
-    schema="public"
-)
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY, options=options)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
